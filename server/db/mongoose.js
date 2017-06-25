d105bffd712078b738eb3666fc58386b33f83442
jitendra <jitendra_stark@hotmail.com>
2017-06-25 16:19:23 +0530

    trying to remove errors

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://jitendra:jitendra@ds149268.mlab.com:49268/todoapp'
-};
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
