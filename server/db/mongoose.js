88f4590f8d5a1b32648203cb1f0bbec6f4f1b5c1
jitendra <jitendra_stark@hotmail.com>
2017-06-25 16:45:22 +0530

    errors!

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://jitendra:jitendra@ds137882.mlab.com:37882/todoapp'
 };
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(db.mlab ||db.localhost );
 
 module.exports = {mongoose};
