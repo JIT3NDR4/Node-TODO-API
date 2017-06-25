ad2babd31fc49d9f28c122fead9da1f49b1b92d2
jitendra <jitendra_stark@hotmail.com>
2017-06-25 14:43:09 +0530

    Added test cases for GET /todos/:id

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,31 +7,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-//   db.collection('Todos').findOneAndUpdate({
-//       _id:new ObjectID('594e918a536fb9386bb6f4fe')
-//   },{
-//       $set:{
-//           completed:true
-//       }
-//   },{
-//       returnOriginal:false
-//   }).then((result)=>{
-//       console.log(result);
-//   });
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
-db.collection('Users').findOneAndUpdate({
-      _id:new ObjectID('594e733c536fb9386bb6f041')
-  },{
-      $set:{
-          name:'ironman'
-      },
-      $inc:{
-        age:1
-      }
-  },{
-      returnOriginal:false
-  }).then((result)=>{
-      console.log(result);
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
   });
 
   // db.close();
